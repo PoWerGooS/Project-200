2aa5da95775b9b6174c4f18abeb9fb99683150e0
PoWerGooS <77084057+PoWerGooS@users.noreply.github.com>
2023-08-03 12:52:21 -0400

    Update server.py

--- server.py
+++ server.py
@@ -7,22 +7,24 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip_address = '127.0.0.1'
 port = 8000
 
+nicknames = []
+
 server.bind((ip_address, port))
 server.listen()
 
 list_of_clients = []
 
 questions = [
-     " What is the Italian word for PIE? \n a.Mozarella\n b.Pasty\n c.Patty\n d.Pizza",
+     " What is the Italian word for PIE? \n a.Pizza\n b.Pasty\n c.Patty\n d.Mozzarella",
      " Water boils at 212 Units at which scale? \n a.Fahrenheit\n b.Celsius\n c.Rankine\n d.Kelvin",
-     " Which sea creature has three hearts? \n a.Dolphin\n b.Octopus\n c.Walrus\n d.Seal",
+     " Which sea creature has three hearts? \n a.Octopus\n b.Dolphin\n c.Walrus\n d.Seal",
      " Who was the character famous in our childhood rhymes associated with a lamb? \n a.Mary\n b.Jack\n c.Johnny\n d.Mukesh",
      " How many bones does an adult human have? \n a.206\n b.208\n c.201\n d.196",
      " How many wonders are there in the world? \n a.7\n b.8\n c.10\n d.4",
      " What element does not exist? \n a.Xf\n b.Re\n c.Si\n d.Pa",
 ]
 
-answers = ['d', 'a', 'b', 'a', 'a', 'a', 'a']
+answers = ['a', 'a', 'a', 'a', 'a', 'a', 'a']
 
 print("Server has started...")
 
@@ -37,7 +39,7 @@ def remove_question(index):
     questions.pop(index)
     answers.pop(index)
 
-def clientthread(conn):
+def clientthread(conn, nickname):
     
     score = 0
     conn.send("Welcome to this quiz game!".encode('utf-8'))
@@ -67,7 +69,10 @@ def remove(connection):
 
 while True:
     conn, addr = server.accept()
+    conn.send('NICKNAME'.encode('utf-8'))
+    nickname = conn.recv(2048).decode('utf-8')
     list_of_clients.append(conn)
+    nicknames.append(nickname)
     print (addr[0] + " connected:)")
-    new_thread = Thread(target= clientthread,args=(conn))
+    new_thread = Thread(target= clientthread,args=(conn, nickname))
     new_thread.start()

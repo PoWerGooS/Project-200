1fa0efa68d6ab9649cbc7a9b71cc9b2815cf1cef
PoWerGooS <77084057+PoWerGooS@users.noreply.github.com>
2023-08-03 12:52:48 -0400

    Update quiz_server.py

--- quiz_server.py
+++ quiz_server.py
@@ -9,7 +9,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip_address = '127.0.0.1'
 port = 8000
 
-client.connect (ip_address , port)
+client.connect ((ip_address , port))
 
 print ("Connected with the server ....")
 
